97becf675e7585186d1fa9c8cf78a395c99cc50d
Andy Read <andyreadpnw@gmail.com>
2020-02-17 20:46:00 -0800

    Updates

---
+++ precache-manifest.723083cfd734896e0ae8e693e22d752d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d84112c4ad5a60d5c2465f5afb706ac",
+    "url": "/battlecry3/index.html"
+  },
+  {
+    "revision": "64e38446e46cedaa1ece",
+    "url": "/battlecry3/static/css/main.85936296.chunk.css"
+  },
+  {
+    "revision": "577352dd0c1aecdbbf47",
+    "url": "/battlecry3/static/js/2.0e7b3f09.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/battlecry3/static/js/2.0e7b3f09.chunk.js.LICENSE"
+  },
+  {
+    "revision": "64e38446e46cedaa1ece",
+    "url": "/battlecry3/static/js/main.9b6725d8.chunk.js"
+  },
+  {
+    "revision": "60063f2d3725ec37e43e",
+    "url": "/battlecry3/static/js/runtime-main.0282c79d.js"
+  }
+]);
